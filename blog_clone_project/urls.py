1adcc6815e3d23255c8b1b1b89da8f3354a52d7d
josepsa <sajan.joseph@tecnotree.com>
2020-04-23 15:00:50 +0000

    removed path import

--- blog_clone_project/urls.py
+++ blog_clone_project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import  include
 
 
 urlpatterns = [
